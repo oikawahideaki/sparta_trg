3b79cf7754c5786d50208a3984e165c55d29240b
及川秀明 <ym949pgd72f@gmail.com>
2015-06-27 20:14:10 +0900

    ユーザー削除機能ボタンを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-def index
+  def index
     @users = User.all
   end
 
@@ -13,13 +13,33 @@ def index
     if @user.save
       redirect_to @user
     else
-      render:new
+      render :new
     end
   end
 
-def show
+  def show
+      @user = User.find(params[:id])
+  end
+
+  def edit
+      @user = User.find(params[:id])
+  end
+
+  def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render :edit
+      end
+  end
+
+  def destroy
     @user = User.find(params[:id])
-end
+    @user.destroy
+    redirect_to users_url
+  end
+
 
   private
 
