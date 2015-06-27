1b2aead9c512eb2de78d56389a2b95446ccec878
及川秀明 <ym949pgd72f@gmail.com>
2015-06-27 18:46:33 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+validates :user_name,
+    presence: true, length: { maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,
+    presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
