1f4fbd075c1b4c15302f4f2c76351d4b5dfa784b
及川秀明 <ym949pgd72f@gmail.com>
2015-06-28 17:22:02 +0900

    レストランのviewの生成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+  def create
+    @restaurant = new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def restaurant_params
+      params.require(:restaurant).permit(
+        :shop_name,
+        :tel,
+        :address,
+        :hp
+        )
+  end
+
+
+
 end
+
